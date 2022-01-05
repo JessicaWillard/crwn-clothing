7be2848ca628e18c824728b403be97445b401704
“Jessica <Jessica.bwillard@okam.ca>
2022-01-05 13:47:56 -0500

    add routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
-import HomePage from './homepage.component';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import HomePage from './pages/homepage/homepage.component';
+import Hats from './pages/hats/hats.component';
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    // <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/hats' element={<Hats />} />
+    </Routes>
+    // </BrowserRouter>
   );
 }
 
---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import MenuItem from '../menu-item/menu-item.component';
+
+import './directory.styles.scss';
+
+class Directory extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sections: [
+        {
+          id: 1,
+          title: 'hats',
+          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+          link: '/hats',
+        },
+        {
+          id: 2,
+          title: 'jackets',
+          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+          link: '',
+        },
+        {
+          id: 3,
+          title: 'sneakers',
+          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+          link: '',
+        },
+        {
+          id: 4,
+          title: 'womens',
+          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+          size: 'large',
+          link: '',
+        },
+        {
+          id: 5,
+          title: 'mens',
+          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+          size: 'large',
+          link: '',
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className='directory-menu'>
+        {this.state.sections.map(({ title, imageUrl, id, size }) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Directory;
